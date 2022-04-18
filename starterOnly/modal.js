de43d948871a413ceedc6707df13913ea96d2d1f
angelique_rosin <angie1979.capucine@yahoo.fr>
2022-04-18 21:34:08 +0200

    ajout placeholder sur les inputs

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -17,11 +17,11 @@ const modalConfirmation = document.querySelector(".formConfirmation");
 const spanValidModal = document.querySelector(".formConfirmation > span");
 
 //On pointe les inputs
-//const inputs = document.querySelectorAll('input[type="text"],input[type="email"], input[type="date"], input[type="number"]');
 const inputs = document.querySelectorAll(
-  "#first, #last, #email, #birthdate, #quantity, #checkbox1"
+  "#first, #last, #email, #birthdate, #quantity, #checkbox1, .checkbox-label"
 );
 
+/*****************Modal*************/
 // open modal
 function launchModal() {
   modalbg.style.display = "block"; /*pour faire apparaitre la modale*/
@@ -45,24 +45,23 @@ closeBtn.addEventListener("click", closeModal);
 // btnSubmit.addEventListener('click', launchModal1)
 
 /****************Le formulaire******************/
-//cgv checked
-const errorDisplay = document.querySelector(".formData > div");
-const check = document.querySelector(".checkbox1");
-const checkbox1 = document.querySelector("#checkbox1");
+
 
 form.addEventListener("submit", function (e) {
   //on empêche le rechargement de la page
   e.preventDefault();
-
-  //cgv checked
-  // if (!checkbox1.checked) {
-  //   check.classList.add("error");
-  //   errorDisplay.textContent =
-  //     "Vous devez vérifier que vous acceptez les termes et conditions.";
-  // }
-
-  if (firstChecker === true && lastChecker === true && emailChecker === true && birthdateChecker === true && quantityChecker === true && checkboxChecker===true) {
+console.log(firstChecker(),lastChecker(),emailChecker());
+  if (
+    firstChecker() === true &&
+    lastChecker() === true &&
+    emailChecker() === true &&
+    birthdateChecker() === true &&
+    quantityChecker() === true &&
+    checkboxChecker() === true &&
+    checkboxContainer() === true
+  ) {
     btnSubmit.style.display = "none";
+   
   }
 
   //   vider les champs une fois qu'on a appuyé sur "valider"
@@ -72,17 +71,13 @@ form.addEventListener("submit", function (e) {
 /*********function firstname (first) ***********/
 const firstChecker = (value) => {
   const firstContainer = document.querySelector(".first-container");
+  console.log(firstContainer);
   const errorDisplay = document.querySelector(".first-container > span");
   //Est ce que le prénom fait plus de 0 caractères ou moins de 2 caractères?
   if (value.length > 0 && value.length < 2) {
     firstContainer.classList.add("error");
     errorDisplay.textContent =
       "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
-    //Si il y a un caractère spéciale
-  } else if (!value.match(/^[a-zA-Z0-9_.-]*$/)) {
-    firstContainer.classList.add("error");
-    errorDisplay.textContent =
-      "Le prénom ne doit pas contenir de caractères spéciaux";
   } else {
     firstContainer.classList.remove("error");
     errorDisplay.textContent = ""; //on vide le texte qui dit l'erreur
@@ -98,10 +93,6 @@ const lastChecker = (value) => {
     lastContainer.classList.add("error");
     errorDisplay.textContent =
       "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
-  } else if (!value.match(/^[a-zA-Z0-9_.-]*$/)) {
-    lastContainer.classList.add("error");
-    errorDisplay.textContent =
-      "Le nom ne doit pas contenir de caractères spéciaux.";
   } else {
     lastContainer.classList.remove("error");
     errorDisplay.textContent = ""; //on vide le texte qui dit l'erreur
@@ -125,11 +116,11 @@ const emailChecker = (value) => {
 /*********function birthdate ***********/
 
 const birthdateChecker = (value) => {
-  const birthdateContainer = document.querySelector (".birthdate-container");
-  const errorDisplay = document.querySelector (".birthdate-container > span");
+  const birthdateContainer = document.querySelector(".birthdate-container");
+  const errorDisplay = document.querySelector(".birthdate-container > span");
 
-  // if (!value.match(/^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/)) {
-    if (value == "") {
+   //if (!value.match(/^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/)) {
+   if (!value) {
     birthdateContainer.classList.add("error");
     errorDisplay.textContent = "Veuillez entrer une date de naissance.";
   } else {
@@ -148,7 +139,7 @@ const quantityChecker = (value) => {
   const quantityContainer = document.querySelector(".quantity-container");
   const errorDisplay = document.querySelector(".quantity-container > span");
 
-  if (value == "") {
+  if (!value) {
     quantityContainer.classList.add("error");
     errorDisplay.textContent = "Veuillez entrer un chiffre.";
   } else {
@@ -157,16 +148,35 @@ const quantityChecker = (value) => {
   }
 };
 
-//cgv checked
+/**********cgv checked*************/
+const errorDisplay = document.querySelector(".formData > div");
+const check = document.querySelector(".checkbox1");
+const checkbox1 = document.querySelector("#checkbox1");
+
 const checkboxChecker = (value) => {
-if (!checkbox1.checked) {
-  check.classList.add("error");
-  errorDisplay.textContent =
-    "Vous devez vérifier que vous acceptez les termes et conditions.";
-} else {
+  if (!checkbox1.checked) {
+    check.classList.add("error");
+    errorDisplay.textContent =
+      "Vous devez vérifier que vous acceptez les termes et conditions.";
+  } else {
     check.classList.remove("error");
     errorDisplay.textContent = "";
   }
+};
+
+/*************checkbox************* */
+ 
+
+const checkboxContainer = (value) => {
+  const errorDisplay = document.querySelector(".formData > small");
+  const checkboxInputs = document.queryselector(".checkbox-inputs")
+
+  if (!checkboxInputs.checked) {
+    checkboxInputs.classList.add("error");
+    errorDisplay.textContent = "Veuillez sélectionner un choix.";
+  } else {
+    errorDisplay.textContent = "";
+  };
 }
 /********************For each*************/
 //On créé un for each pour pointer tous les inputs et pour évoluer dans chacun d'eux
@@ -194,6 +204,8 @@ inputs.forEach((input) => {
       case "checkbox1":
         checkboxChecker(e.target.value);
         break;
+      case ".checkbox-label" :
+        checkboxContainer(e.target.value);
       default:
         nul;
     }

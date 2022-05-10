1e162531f8c8a1d29cfa896212dd73e44b02cc08
angelique_rosin <angie1979.capucine@yahoo.fr>
2022-05-10 23:15:32 +0200

    modification de la fonction fermeture modale de confirmation inscription, fait en js au lieu de on click en html

--- scripts/modalConfirmation.js
+++ scripts/modalConfirmation.js
@@ -1,6 +1,8 @@
 const btnSubmit = document.querySelector(".btn-submit");
 const modalConfirmation = document.querySelector(".formConfirmation");
 const spanValidModal = document.querySelector(".formConfirmation > span");
+const btnSubmitConfirm = document.querySelector(".btn-submit-confirm");
+
 
 //Faire apparaitre la modale de confirmation d'inscription
 function launchModal1() {
@@ -9,3 +11,10 @@ function launchModal1() {
   }
   
   btnSubmit.addEventListener("click", launchModal1);
+
+  // close modal
+function closeModal() {
+  modalbg.style.display = "none"; /*pour faire disparaitre la modale*/
+}
+
+btnSubmitConfirm.addEventListener("click", closeModal);

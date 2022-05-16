00527ca18cd1c700a43164c25e864549af337a3b
angelique_rosin <angie1979.capucine@yahoo.fr>
2022-05-16 23:21:54 +0200

    modification du nom de la fonction launchModalConfirmation

--- scripts/modalConfirmation.js
+++ scripts/modalConfirmation.js
@@ -3,23 +3,22 @@ const modalConfirmation = document.querySelector(".formConfirmation");
 const spanValidModal = document.querySelector(".formConfirmation > span");
 const btnSubmitConfirm = document.querySelector(".btn-submit-confirm");
 
-
 /**
  * Fonction pour ouvrir la modale de confirmation d'inscription
  */
 function launchModalConfirmation() {
-    modalConfirmation.style.display = "block";
-    spanValidModal.innerHTML = "Merci pour <br> votre inscription";
-  }
-  
-  btnSubmit.addEventListener("click", launchModal1);
+  modalConfirmation.style.display = "block";
+  spanValidModal.innerHTML = "Merci pour <br> votre inscription";
+}
 
-  /**
-   * Fonction pour fermer la modale de confirmation d'inscription
-   */
+btnSubmit.addEventListener("click", launchModalConfirmation);
+
+/**
+ * Fonction pour fermer la modale de confirmation d'inscription
+ */
 function closeModalConfirmation() {
-  modalbg.style.display = "none"; 
+  modalbg.style.display = "none";
   window.location.reload();
 }
 
-btnSubmitConfirm.addEventListener("click", closeModal1);
+btnSubmitConfirm.addEventListener("click", closeModalConfirmation);
